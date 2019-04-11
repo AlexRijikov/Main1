98943408cab9c861a49a7ef5ef199f5d6b299525
Zver <rijikov>
2019-04-11 21:04:39 +0300

    mty

---
+++ src/ua/zp/brainacad/ItemThree.java
@@ -0,0 +1,16 @@
+package ua.zp.brainacad;
+
+/**
+ * This is my First java experience.
+ * test
+ *
+ * test
+ *
+ * @seeSystem
+ */
+public class ItemThree {
+    public static void main(String[] args) {
+        System.out.print("\nH"+"\nE"+"\nL"+"\nL"+"\nO"+
+                "\n"+"\tW"+"\tO"+"\tR"+"\tL"+"\tD");
+    }
+}
